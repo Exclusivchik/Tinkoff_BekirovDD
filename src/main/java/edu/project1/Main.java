ce21cddec0808ba1f4d5b50e321ac2f1997b7b2f
Damirem <damirem2004@mail.ru>
2023-10-21 00:29:18 +0300

    Закоментил мейн

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -4,8 +4,9 @@ public final class Main {
     private Main() {
     }
 
-    public static void main(String[] args) {
-        HangMan game = new HangMan("~cin", RandomWord.get());
-        game.run();
-    }
+//    public static void main(String[] args) {
+//        String word = RandomWord.get();
+//        HangMan game = new HangMan("~cin", word);
+//        game.run();
+//    }
 }
