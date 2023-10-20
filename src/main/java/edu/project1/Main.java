c696e033ded3b6c7fd30fbd5196b477c7f05444b
Damirem <damirem2004@mail.ru>
2023-10-21 00:37:08 +0300

    Убрал final из RandomWord

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -4,9 +4,9 @@ public final class Main {
     private Main() {
     }
 
-//    public static void main(String[] args) {
-//        String word = RandomWord.get();
-//        HangMan game = new HangMan("~cin", word);
-//        game.run();
-//    }
+    public static void main(String[] args) {
+        String word = RandomWord.get();
+        HangMan game = new HangMan("~cin", word);
+        game.run();
+    }
 }
