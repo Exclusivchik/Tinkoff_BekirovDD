0a3d2e902c4c35fc2f9cbeeab80f0839dd7b1a83
Damirem <damirem2004@mail.ru>
2023-10-26 13:35:12 +0300

    Дз3

--- src/main/java/edu/hw3/Task3.java
+++ src/main/java/edu/hw3/Task3.java
@@ -17,18 +17,4 @@ public class Task3 {
 
         return freqMap;
     }
-
-    public static void main(String[] args) {
-        List<String> list1 = List.of("a", "bb", "a", "bb");
-        System.out.println(freqDict(list1)); // Output: {bb=2, a=2}
-
-        List<String> list2 = List.of("this", "and", "that", "and");
-        System.out.println(freqDict(list2)); // Output: {that=1, and=2, this=1}
-
-        List<String> list3 = List.of("код", "код", "код", "bug");
-        System.out.println(freqDict(list3)); // Output: {код=3, bug=1}
-
-        List<Integer> list4 = List.of(1, 1, 2, 2);
-        System.out.println(freqDict(list4)); // Output: {1=2, 2=2}
-    }
 }
