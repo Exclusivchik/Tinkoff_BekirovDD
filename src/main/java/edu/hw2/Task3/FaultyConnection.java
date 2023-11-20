76ace8032ad3129eea3ff2f904a90bdb7a0fe245
Damirem <damirem2004@mail.ru>
2023-11-20 21:40:50 +0300

    опять...

--- src/main/java/edu/hw2/Task3/FaultyConnection.java
+++ src/main/java/edu/hw2/Task3/FaultyConnection.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 
 public class FaultyConnection implements Connection {
     private final static Logger LOGGER = LogManager.getLogger();
-    private static final double FAILURE_PROBABILITY = 0.99999;
+    private static final double FAILURE_PROBABILITY = 1;
 
     @Override
     public void execute(String command) throws ConnectionException {
