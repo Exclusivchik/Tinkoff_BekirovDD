c5d0a3b6de803e9ce9745d8c0d11e472d177c227
Damirem <damirem2004@mail.ru>
2023-12-15 20:21:44 +0300

    наработки

---
+++ src/main/java/edu/hw10/Task1/Annots/Min.java
@@ -0,0 +1,12 @@
+package edu.hw10.Task1.Annots;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Min {
+    int value();
+}
